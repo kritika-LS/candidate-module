41c65a6349ba627966cd1f97c0a1d03bdcfa6d8c
kritika-LS <Kritika.J@lancesoft.com>
2025-05-19 22:59:19 +0530

    Minor change

--- src/screens/ProfileScreen/ProfessionalInformation/index.tsx
+++ src/screens/ProfileScreen/ProfessionalInformation/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-export const ProfessionalInformationSextion = () => {
+export const ProfessionalInformationSection = () => {
     return (
         <View>
             <Text>Professional Information</Text>
